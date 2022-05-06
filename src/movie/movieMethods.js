75ab4f74cf891cf8f9319f35b4b6ae08b6b49b85
Monique Jackson <bymoniquejackson@gmail.com>
2022-05-06 12:10:05 +0100

    CRUDE COMPLETE!

--- src/movie/movieMethods.js
+++ src/movie/movieMethods.js
@@ -13,12 +13,28 @@ exports.addMovie = async (movieObj) => {
 }
 
 
-
-
-exports.listMovies = async () => {
+exports.listMovie = async (movieObj) => {
     try {
-        return await Movie.findAll();
+        return await Movie.findAll(movieObj);
     } catch (error) {
             console.log(error)
         }
     }
+
+
+    exports.updateMovie = async (movieObj) => {
+        try {
+            await Movie.update(
+            {title: movieObj.title, actor: movieObj.actor},
+            {where: {title: movieObj.title}}
+            );
+        } catch (error) {
+                console.log(error)
+            }
+        }
+    
+        exports.destroyMovie = async (movieObj) => {
+            await Movie.destroy({
+                where: {title: movieObj.title}
+            })
+        }
