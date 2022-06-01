167dd3dda27561bee0d586be9509dd43c67e56d7
Monique Jackson <bymoniquejackson@gmail.com>
2022-06-01 09:15:59 +0100

    actors

--- src/movie/actorMethods.js
+++ src/movie/actorMethods.js
@@ -1,46 +1,48 @@
-// const Movie = require ("./movieTable")
-// const Actor = rewuire ("./actorTable")
+const Movie = require ("./movieTable")
+const Actor = require ("./actorTable")
 
 
 
 
 
-// exports.addActor = async (actorObj) => {
-//     try {
-//         await Actor.create(actorObj);
-//     } catch (error) {
-//         console.log(error)
-//     }
-// }
+exports.addActor = async (actorObj) => {
+    try {
+        await Actor.create(actorObj);
+    } catch (error) {
+        console.log(error)
+    }
+}
 
 
-// exports.listMovie = async () => {
-//     try {
-//         return await Actor.findAll();
-//     } catch (error) {
-//             console.log(error)
-//         }
-//     }
+exports.listMovie = async () => {
+    try {
+        return await Actor.findAll();
+    } catch (error) {
+            console.log(error)
+        }
+    }
 
 
-//     exports.updateActor = async () => {
-//         try {
-//             await Actor.update(
-//             {name: actorObj.actor},
-//             {where: {title: actorObj.title}}
-//             );
+    exports.updateActor = async () => {
+        try {
+            await Actor.update(
+            {name: actorObj.actor},
+            {where: {title: actorObj.title}}
+            );
 
 
 
-//         exports.deleteActor = async (actorObj) => {
-//             await Actor.destroy({
-//                 where: {title: actor}
-//                 })
-//             }
+        exports.deleteActor = async (actorObj) => {
+            await Actor.destroy({
+                where: {title: Actor}
+                })
+            }
 
 
-//         } catch (error) {
-//                 console.log(error)
-//             }
-//         }
+        } catch (error) {
+                console.log(error)
+            }
+        }
     
+
+    //rework
